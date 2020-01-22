41a50e78a6fab16dd085f4eb3b14025d3eb5f7bf
Kazu <nkz9528w.econ@gmail.com>
2020-01-22 21:18:14 +0900

    Init

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import {render} from 'react-dom'
+import StickyHeadTable from './Table'
+
+render(
+<div>
+  <StickyHeadTable>
+  </StickyHeadTable>
+</div>,
+
+document.getElementById('app'))
